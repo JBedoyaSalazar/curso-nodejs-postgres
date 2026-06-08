cd670df700667bf5a579b0d070886bba26115126
Jefred Bedoya <jefred03salazar@gmail.com>
2026-06-08 01:17:08 -0500

    feat: add OrderProduct model integration; establish many-to-many relationship between Order and Product

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,7 @@ import { CustomerSchema, Customer } from './customers.model.js'
 import { ProductSchema, Product } from './product.model.js'
 import { CategorySchema, Category } from './category.model.js'
 import { OrderSchema, Order } from './order.model.js'
+import { OrderProcductSchema, OrderProduct } from './order-product.model.js'
 
 export function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize))
@@ -10,10 +11,12 @@ export function setupModels(sequelize) {
   Product.init(ProductSchema, Product.config(sequelize))
   Category.init(CategorySchema, Category.config(sequelize))
   Order.init(OrderSchema, Order.config(sequelize))
+  OrderProduct.init(OrderProcductSchema, OrderProduct.config(sequelize))
 
   User.associate(sequelize.models)
   Customer.associate(sequelize.models)
   Category.associate(sequelize.models)
   Product.associate(sequelize.models)
   Order.associate(sequelize.models)
+  
 }
--- db/models/order.model.js
+++ db/models/order.model.js
@@ -36,6 +36,12 @@ class Order extends Model {
       as: 'customer',
       foreignKey: 'customerId'
     })
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId'
+    })
   }
 
   static config(sequelize) {
