89de918bfdb5329dad9ec5949ba5fdff1c59795d
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-02 14:01:18 -0500

    feat: add SMTP configuration and implement email sending with Nodemailer

--- config/config.js
+++ config/config.js
@@ -7,5 +7,11 @@ export const config = {
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
   apiKey: process.env.API_KEY,
-  jwtSecret: process.env.JWT_SECRET
+  jwtSecret: process.env.JWT_SECRET,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  SMTP_FROM: process.env.SMTP_FROM,
+  SMTP_TO: process.env.SMTP_TO
 }
---
+++ examples/nodemailer.js
@@ -0,0 +1,50 @@
+import nodemailer from 'nodemailer';
+import { config } from '../config/config.js';
+
+async function sendMail() {
+  try {
+    console.log({
+      host: config.SMTP_HOST,
+      port: config.SMTP_PORT,
+      user: config.SMTP_USER,
+    });
+
+    const transporter = nodemailer.createTransport({
+      host: config.SMTP_HOST,
+      port: Number(config.SMTP_PORT),
+      secure: true,
+      auth: {
+        user: config.SMTP_USER,
+        pass: config.SMTP_PASSWORD,
+      },
+    });
+
+    await transporter.verify();
+
+    console.log('✅ Conexión SMTP exitosa.');
+
+    const info = await transporter.sendMail({
+      from: config.SMTP_FROM,
+      to: config.SMTP_TO,
+      subject: 'Un Correo para mi bebe preciosa',
+      text: 'Este es un correo enviado desde Nodemailer.',
+      html: `
+        <h1>Correo de prueba</h1>
+        <p>Si recibiste este correo, Nodemailer está funcionando correctamente.</p>
+        <h1 style="color: #e64a4a;">¡Hola, mi amor!</h1>
+        <p>Este es un correo de prueba enviado desde Nodemailer.</p>
+        <p>¡Espero que tengas un día maravilloso!</p>
+        <p>Con cariño,</p>
+        <p>Tu amorcito</p>
+        <p>❤️</p>
+      `,
+    });
+
+    console.log('Correo enviado.');
+    console.log('Message ID:', info.messageId);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+sendMail();
